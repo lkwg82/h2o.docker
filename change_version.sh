2354efaa3897a0a4dc7f6c181849b91f08a14b4b
Lars K.W. Gohlke <lkwg82@gmx.de>
2016-02-13 00:00:05 +0100

    removed trigger

--- change_version.sh
+++ change_version.sh
@@ -26,16 +26,3 @@ fi
 
 git push 
 git push --tags --force
-
-exit
-function trigger {
-    local data=$1
-    local endpoint="https://registry.hub.docker.com/u/lkwg82/h2o-http2-server/trigger/ed94f4aa-f3b6-40f0-819c-d72d6789c58e/"
-    curl -s -H "Content-Type: application/json" --data "$data" -X POST $endpoint
-}
-
-if [ -n "$tag" ]; then
-    trigger "{'source_type': 'Tag', 'source_name': '$tag', 'docker_tag': 'latest'}"
-else
-    trigger "{'build': true, 'docker_tag': 'daily'}"
-fi
