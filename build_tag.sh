cd08617ebe07bf325732e5a5f4acbe1259d8f0e2
Lars K.W. Gohlke <lkwg82@gmx.de>
2017-01-19 08:21:47 +0100

    refactored to build single version

---
+++ build_tag.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+tag=$1
+
+latest=$(tail -n1 tagged.versions)
+image="lkwg82/h2o-http2-server:$tag"
+docker build --no-cache --tag $image https://github.com/lkwg82/h2o.docker.git#$tag 
+docker push $image
+if [[ "$latest" == "$tag" ]]; then
+   docker tag lkwg82/h2o-http2-server:$latest lkwg82/h2o-http2-server:latest
+   docker push lkwg82/h2o-http2-server:latest
+   docker rmi lkwg82/h2o-http2-server:latest
+fi
+# cleanup local build cache
+docker rmi $image
