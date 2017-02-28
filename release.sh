041369167746d33b07681893b91258c0943d34e0
Lars K.W. Gohlke <lkwg82@gmx.de>
2017-02-28 22:11:34 +0100

    cleanup

--- release.sh
+++ release.sh
@@ -3,7 +3,10 @@
 set -e
 
 git pull
+
 pushd scripts
+
 ./check_releases.sh
 ./update_released_docker_images.sh
+
 popd
--- scripts/check_releases.sh
+++ scripts/check_releases.sh
@@ -12,12 +12,12 @@ function changeVersion {
     tag="$1"
     VERSION="tags/$tag"
 
-    sed -e 's#^\(ENV VERSION \).*#\1 '$VERSION'#' -i Dockerfile
+    sed -e 's#^\(ENV VERSION \).*#\1 '$VERSION'#' -i ../Dockerfile
 
     line='- ```'$tag'```'" (*[$tag/Dockerfile](https://github.com/lkwg82/h2o.docker/blob/$tag/Dockerfile)*)"
-    echo $line >> README.md
+    echo $line >> ../README.md
 
-    git commit -m "changed to version $VERSION" Dockerfile tagged.versions README.md
+    git commit -m "changed to version $VERSION" ../Dockerfile tagged.versions ../README.md
     git tag --force --annotate $tag -m "released version $tag" HEAD 
 
     git push 
@@ -26,13 +26,20 @@ function changeVersion {
 
 apiResult=.api.releases
 curl -s https://api.github.com/repos/h2o/h2o/releases > $apiResult
-releases=$(cat $apiResult | jq '.[] | .published_at + " " + .tag_name'| sed -e 's#"##g' | sort | cut -d\   -f2)
+releases=$(cat $apiResult \
+		| jq '.[] | .published_at + " " + .tag_name' \
+		| sed -e 's#"##g' \
+		| sort \
+		| cut -d\   -f2 \
+		| grep -v "v1." \
+		| grep -v "v2.0" 
+)
 
 for r in $releases; do
     #~ echo -n "checking $r ... "
-    if [ $(grep ^$r$ ../tagged.versions | wc -l) -eq 0 ]; then
+    if [ $(grep ^$r$ tagged.versions | wc -l) -eq 0 ]; then
         echo " tagging '$r'"
-        echo $r >> ../tagged.versions
+        echo $r >> tagged.versions
         changeVersion $r
         touch new_version
     #~ else
