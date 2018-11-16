4aa9cfccfee5b0d3ca275eec2b9bd321d2c2537a
Lars K.W. Gohlke <lkwg82@gmx.de>
2018-11-16 14:37:46 +0100

    kills running test containers

--- test/mruby/test.sh
+++ test/mruby/test.sh
@@ -7,6 +7,8 @@ function finish {
 
 	echo "cleanup"
         docker image rm -f test-buddy >/dev/null
+        docker kill $(cat .cid)
+        rm -f .cid
 
 	echo "---------"	
 	if [ "$exitCode" == "0" ]; then
@@ -22,19 +24,19 @@ trap finish EXIT
 # build image
 cd ../..
 echo -n "building image ... "
-docker build -t test-h2o . > /dev/null
+docker build -t test-h2o . #> /dev/null
 echo ok
 cd $OLDPWD
 
 docker build -t test-buddy -f Dockerfile.test . >/dev/null
 
-cid=$(docker run --rm -d \
+docker run --cidfile .cid --rm -d \
 	--cap-drop ALL \
 	--cap-add SETUID \
 	--cap-add SETGID \
 	--cap-add SYS_ADMIN \
-	test-buddy)
-ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $cid)
+	test-buddy
+ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(cat .cid))
 
 sleep 1
 
